08deae4c4eb9d5be3b9c6e0dca4eb6dc9569fc9b
rzaqa <qarfox@gmail.com>
2025-06-26 18:00:31 +0200

    gh-page branch - deleted files

--- 01_intro/07_test_drag_and_drop.py
+++
@@ -1,6 +0,0 @@
-def test_drag_and_drop(page):
-    page.goto('https://zimaev.github.io/draganddrop/')
-    page.drag_and_drop("#drag", "#drop")
-
-
-
--- 04_pom/pages/__init__.py
+++
@@ -1,2 +0,0 @@
-from .login_page import LoginPage
-from .dashboard_page import DashboardPage
